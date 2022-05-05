f7a791e15c9c7c2f550c7e8e7e750ca8ab8c507b
我691044779 <1691044779@qq.com>
2022-05-05 22:57:54 +0800

    Site updated: 2022-05-05 22:57:53

---
+++ js/weater.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "4A86E8",
+    "citySize": "16",
+    "aqiColor": "FF9900",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "10",
+    "top": "10",
+    "key": "51c50fee13d648818a0c9adca1233c97"
+  }
+}
